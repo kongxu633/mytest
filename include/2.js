325e67534a0792e3c9d32fc9531b1adce0db245f
kongxu633 <kongxu633@gmail.com>
2015-04-20 17:42:19 +0800

    选择其他 显示input
    需要解决 cateprops里面的 input 中文字段不显示

--- include/2.js
+++ include/2.js
@@ -2,7 +2,7 @@ $(document).ready(function(e) {
 	//初始化数值
     defChecked();
 	
-	//defHidden();
+	defHidden();
 	specialHidden();
 	//选择后自动写入
 	select2input();
@@ -10,6 +10,20 @@ $(document).ready(function(e) {
 	input2select();
 	
 	//hookSubmit();
+	$('#getCateProps').click(function(){
+		$("#cateProps").empty();
+		$("#inputPids").empty();
+		$('[name^=cp_]').each(function(index, element) {
+            $("#cateProps").append($(this).val()+';');
+        });
+		$("#inputValues").append($('#J_MainForm').serialize());
+		x=$('#J_MainForm').serializeArray();
+		$.each(x, function(i, field){
+			if(field.value != ''){
+				$("#inputPids").append(field.value + ';');
+			}
+		});
+	});
 });	
 
 function defChecked(){
@@ -58,7 +72,7 @@ function defChecked(){
 
 function defHidden(){
 		$('[id^=simulate-prop_]').each(function(index, element) {
-            console.log($(this).attr('id')+'  ---  '+$(this).val());
+            //console.log($(this).attr('id')+'  ---  '+$(this).val());
 			$(this).css({'display':'none', 'visibility':'hidden'});
         });
 }
@@ -66,6 +80,7 @@ function defHidden(){
 function specialHidden(){
 			//商品条形码
 			$('#spu_2814443').css({'display':'none', 'visibility':'hidden'});
+			//货号
 			$('#ariaby-prop_13021751').parent().css({'display':'none', 'visibility':'hidden'});
 }
 
@@ -85,17 +100,32 @@ function select2input(){
 			//获取select选中的 value: $("#ddlregtype ").val(); 
 			//获取select选中的索引: $("#ddlregtype ").get(0).selectedindex;
 			inputid = '#simulate-' + $(this).attr('id');
-			$(inputid).val( $(this).find("option:selected").text().trim() );
-			
+			$(inputid).val( $(this).find("option:selected:not(.J_other)").text().trim() );
+			$(inputid).val() == '' ? $(inputid).css({'display':'', 'visibility':''}).show().focus() : $(inputid).css({'display':'none', 'visibility':'hidden'}) ;			
+			/*
+			inputtext = $(this).find("option:selected").hasClass('J_other') ? '' : $(this).find("option:selected").text().trim();
+			inputtext == '' ? $(inputid).attr('visibility','show').show().focus() : $(inputid).val( inputtext );
+			*/
+			/*
+			$(inputid).val( $(this).find("option:selected:not(.J_other)").text().trim() );
+			$(inputid).val() == '' ? $(inputid).css({'display':'', 'visibility':''}).show().focus() : $(inputid).css({'display':'none', 'visibility':'hidden'}) ;
+			*/
+			//选中棉的二级分类
+			if($(this).val() == '20021:105255'){selectCotton($(this));}
 		});
 }
+
+function selectCotton(e){
+	//alert(e.val());
+}
+
 
 function input2select(){
 		//选择器还需要完善
 		$('input').blur(function() {
 			inputid = $(this).attr('id');
 			if(inputid == 'simulate-prop_20000' || inputid == 'simulate-prop_13021751' || inputid == 'simulate-prop_9698958' || inputid == 'simulate-prop_9938993'){
-					//这里需要将select选中默认值
+					//这里需要将select选中默认值 不等于空的时候需要把select重置到其他
 					($(this).val() == '') ? true : true;
 			}
 		
